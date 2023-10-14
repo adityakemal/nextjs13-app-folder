dc1b497f07388754a480c8b8783069fc9322759a
Kemal Zulfiqar <kemalzulfiqar@Kemals-MacBook-M1-Pro.local>
2023-10-15 03:20:32 +0700

    fix build

---
+++ .next/static/chunks/app/product/error-e4d268ae2cf96728.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[268],{903:function(e,r,t){Promise.resolve().then(t.bind(t,7410))},7410:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return errorPageProduct}});var n=t(7437),s=t(2265);function errorPageProduct(e){let{error:r,reset:t}=e;return(0,s.useEffect)(()=>{console.log(r)},[r]),(0,n.jsxs)("div",{className:" flex h-screen w-screen justify-center items-center",children:[" ",(0,n.jsxs)("div",{className:"",children:[(0,n.jsx)("h1",{className:"text-[60px]",children:"ERROR PRODUCT PAGE HERE"}),(0,n.jsx)("button",{className:"bg-red-400 px-3 py-2",onClick:()=>t(),children:"reset"})]})]})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),s=Symbol.for("react.element"),o=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),c=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,f={},i=null,l=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(l=r.ref),r)o.call(r,n)&&!u.hasOwnProperty(n)&&(f[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===f[n]&&(f[n]=r[n]);return{$$typeof:s,type:e,key:i,ref:l,props:f,_owner:c.current}}r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=903)}),_N_E=e.O()}]);
---
+++ .next/static/chunks/app/profile/page-abdd1c52f56cf1c6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[178,555,301,869,566,1,489,925,386,175,403,249,136,964],{3521:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
