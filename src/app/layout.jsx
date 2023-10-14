e33974ff095214ca43fe1808ce1bcfb3f27cc9d9
Kemal Zulfiqar <kemalzulfiqar@Kemals-MacBook-M1-Pro.local>
2023-10-15 04:12:38 +0700

    add meta google robot

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,6 +21,19 @@ export const metadata = {
   icons: {
     icon: "/next.svg",
   },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 // const myfont = localFont({src : './'})
